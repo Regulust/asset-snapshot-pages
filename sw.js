f4e4d774064f8a93faf69f9c8adcd9a62aa653ca
Regulust <21237826+Regulust@users.noreply.github.com>
2026-06-30 01:14:30 +0200

    Tighten heatmap controls and bump cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "asset-snapshot-book-v126";
+const CACHE_NAME = "asset-snapshot-book-v127";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
